7b2da17617139e42a6be119b76cc891fb3abfb4e
TravLincoln <Travis.dlincoln@gmail.com>
2021-10-13 11:49:41 -0700

    First commit

---
+++ calculator.py
@@ -0,0 +1,23 @@
+def calculator(num1,num2,operator):
+    if operator =='+':
+        return float(num1) + float(num2)
+
+    elif operator == '-':
+        return float(num1) -  float(num2)
+
+    elif operator == '*':
+        return float(num1) * float(num2)
+   
+    elif operator == '/':
+        return float(num1) / float(num2)
+
+    elif operator == "**":
+        return float(num1)** float(num2)
+
+    else:
+        return False
+
+def parse_input():
+    num1,operator,num2 = input("Enter equation: ").split()
+    print(calculator(num1,num2,operator))
+
---
+++ multiply.py
@@ -0,0 +1,8 @@
+import math
+def multiply_list(myList):
+    
+    result = 1
+    for i in myList:
+        res = res * i 
+    return res
+    
