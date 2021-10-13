9e7c3ef69b56617e7d687d7df54510d8e8e37811
TravLincoln <Travis.dlincoln@gmail.com>
2021-10-13 16:20:39 -0700

    Save1

--- calculator.py
+++ calculator.py
@@ -9,7 +9,7 @@ def calculator(num1,num2,operator):
         return float(num1) * float(num2)
    
     elif operator == '/':
-        return float(num1) / float(num2)
+        return float(num1) / float(num2) if float(num2) != 0 else 0
 
     elif operator == "**":
         return float(num1)** float(num2)
@@ -20,4 +20,3 @@ def calculator(num1,num2,operator):
 def parse_input():
     num1,operator,num2 = input("Enter equation: ").split()
     print(calculator(num1,num2,operator))
-
